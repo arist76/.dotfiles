34b05a720a6cacc1d1ca7d481ead3628d762fb0a
surafel fikru <surafelfikru76@gmail.com>
2024-07-29 15:40:56 +0300

    commit by ishak

---
+++ nvim/nvim/lua/plugins/tab.lua
@@ -0,0 +1,33 @@
+return {
+	"akinsho/bufferline.nvim",
+	requires = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				-- show_tab_indicators = false,
+				separator_style = { "", "" }, -- | "slope" | "slant"| "thin" | "thick" | { 'any', 'any' },
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						text_align = "left",
+						separator = false,
+					},
+				},
+			},
+		})
+		-- RE MPAS
+		vim.opt.termguicolors = true
+
+		-- vim.api.nvim_set_keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+		-- vim.api.nvim_set_keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>w", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
+
+		-- change buffers left and right
+		vim.keymap.set("n", "L", ":bnext<CR>")
+		vim.keymap.set("n", "H", ":bprevious<CR>")
+	end,
+}
