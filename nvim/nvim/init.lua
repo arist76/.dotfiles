20087629263444a1c2799ffacacf1062d685b2d9
surafel_fikru <surafelfikru76@gmail.com>
2025-09-18 12:31:19 +0300

    add tmux an nvim

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,8 @@
+
+vim.o.termguicolors = true
+
+-- Disable the default statusline
+vim.opt.laststatus = 3  -- Global statusline (Neovim 0.7+)
+
+require("config.lazy")
+require("config.vim-options")
---
+++ nvim/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,1 @@
+return { "catppuccin/nvim", name = "catppuccin", flavour="latte", priority = 1000 }
